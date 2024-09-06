19870742e5985054f30e9b1d35552d7874dd4605
mani-prakash-007 <manip8072@gmail.com>
2024-09-06 09:38:53 +0530

    Add Data fetching from API

--- src/components/RecipeFinder.jsx
+++ src/components/RecipeFinder.jsx
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useRef, useState } from "react";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const RecipeFinder = () => {
-    //States
-  const [recipes, setRecipes] = useState;
-  
-  //Function 
-  const handleClick = async() =>{
-    
-  }
+  //Reference
+  const inputRef = useRef();
+  //States
+  const [recipes, setRecipes] = useState();
+
+  //Function
+  const handleClick = async () => {
+    const inputValue = inputRef.current.value;
+    const url = `https://api.edamam.com/search?q=${inputValue}&app_id=248a9ca1&app_key=${API_KEY}&form=0to=10`;
+    const response = await axios.get(url);
+    console.log(response.data);
+  };
 
   return (
     <>
       <div className="flex flex-col justify-center w-full">
         <div className="flex justify-center p-5">
           <input
+            ref={inputRef}
             type="text"
             className="border border-black h-10 px-5 mx-5 w-80 rounded-xl focus:outline-none "
             placeholder="Search for Recipes"
           />
-          <button className="mx-5 px-5 rounded-xl active:scale-90 bg-green-700 font-bold text-white text-xl hover:bg-green-900">
+          <button
+            onClick={handleClick}
+            className="mx-5 px-5 rounded-xl active:scale-90 bg-green-700 font-bold text-white text-xl hover:bg-green-900"
+          >
             Search
           </button>
         </div>
